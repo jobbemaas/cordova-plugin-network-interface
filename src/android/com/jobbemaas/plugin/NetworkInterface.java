22ff42b1d07de43a454758b72da787e732a73491
Jobbe Maas <jobbemaas@gmail.com>
2021-08-05 14:26:06 +0200

    Update NetworkInterface.java

--- src/android/com/jobbemaas/plugin/NetworkInterface.java
+++ src/android/com/jobbemaas/plugin/NetworkInterface.java
@@ -1,5 +1,8 @@
 package com.jobbemaas.plugin;
 
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -9,6 +12,8 @@ import org.json.JSONObject;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 public class NetworkInterface extends CordovaPlugin {
 
@@ -46,16 +51,32 @@ public class NetworkInterface extends CordovaPlugin {
 	public void getHostName() {
 
 		final CallbackContext _callbackContext = callbackContext;
+		private static final String TAG = NetworkInterfaceTest.class.getSimpleName();
+		
+		Enumeration<NetworkInterface> it_ni = NetworkInterface.getNetworkInterfaces();
+		
+		while (it_ni.hasMoreElements()) {
+		    NetworkInterface ni = it_ni.nextElement();
+		    Enumeration<InetAddress> it_ia = ni.getInetAddresses();
+		    if (it_ia.hasMoreElements()) {
+				Log.i(TAG, "++ NI:   " + ni.getDisplayName());
+				while (it_ia.hasMoreElements()) {
+				    InetAddress ia = it_ia.nextElement();
+				    Log.i(TAG, "-- IA:   " + ia.getCanonicalHostName());
+				    Log.i(TAG, "-- host: " + ia.getHostAddress());
+				}
+		    }
+		}
 
 		InetAddress ip;
 		String hostname;
 
 		try {
-            ip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
-            _callbackContext.success(hostname);
-        } catch (UnknownHostException e) {
-        	_callbackContext.error("Error while calling ''" + action + "' '" + e.getMessage());
-        }
+		    ip = InetAddress.getLocalHost();
+		    hostname = ip.getHostName();
+		    _callbackContext.success(hostname);
+		} catch (UnknownHostException e) {
+			_callbackContext.error("Error while calling ''" + action + "' '" + e.getMessage());
+		}
 	}
 }
